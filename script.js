998e9312d891fe9e420707d734482250cfe00944
mrndolo <callistomutunga@gmail.com>
2024-07-01 08:46:06 +0300

    added code for changing the voices

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 let speech = new SpeechSynthesisUtterance();
 
+// changing the voices
+let voices = [];
+
+let voiceSelect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0];
+
+    // display each voice in the dropdown
+    voices.forEach((voice, i)=>(voiceSelect.options[i] = new Option(voice.name, i)));
+}
+
 // speaking functionality triggered by the button
 document.querySelector("button").addEventListener("click", ()=>{
     speech.text = document.querySelector("textarea").value;
